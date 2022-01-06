41e94364af359a748b0a758a0c88d05915fae0bc
Preston Hall <prestonhall31@gmail.com>
2022-01-06 14:28:34 -0800

    Updated open_connection func

--- HelloPluralsightProject/jobs/app.py
+++ HelloPluralsightProject/jobs/app.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template 
+from typing_extensions import ParamSpec
+from flask import Flask, render_template, g
+import sqlite3
+
+PATH = "db/jobs.sqlite"
 
 app = Flask(__name__)
+
+def open_connection():
+    connection = getattr(g,'_connection', None)
+    if connection is None:
+        connection = sqlite3.connect(PATH)
+        g._connection = sqlite3.connect(PATH)
+
+    return connection
 
 @app.route("/")
 @app.route("/jobs")
